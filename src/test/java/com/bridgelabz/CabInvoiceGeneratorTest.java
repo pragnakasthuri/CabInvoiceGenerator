d61bcf91c6ae1724dd212fa56b156fef679501de
pragnakasthruri <kpragna25@gmail.com>
2022-03-25 23:10:37 +0530

    Adding test cases for userId related Invoice Summary

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class CabInvoiceGeneratorTest {
     CabInvoiceGenerator cabInvoiceGenerator = null;
 
@@ -35,6 +37,18 @@ public class CabInvoiceGeneratorTest {
                 new Ride(0.1, 1)
         };
         InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
+    }
+
+    @Test
+    public void givenUserId_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        cabInvoiceGenerator.userRideRepository.put("User001", Arrays.asList(rides));
+        InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User001");
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
         Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
     }
