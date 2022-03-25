2948905ee0a534d7d5876fd376c5d6ce9a676386
pragnakasthruri <kpragna25@gmail.com>
2022-03-25 22:40:40 +0530

    Adding test cases for multiple rides

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -1,15 +1,40 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator cabInvoiceGenerator = null;
+
+    @BeforeEach
+    public void init() {
+        cabInvoiceGenerator = new CabInvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = cabInvoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double totalFare = cabInvoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, totalFare, 0.0);
     }
 }
