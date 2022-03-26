1d3b1fba0e31ee2447163efb55062a0a3d478e25
pragnakasthruri <kpragna25@gmail.com>
2022-03-26 06:59:05 +0530

    Adding the test case for normal and premium rides

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -18,7 +18,7 @@ public class CabInvoiceGeneratorTest {
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time, "NORMAL");
         Assertions.assertEquals(25, fare, 0.0);
     }
 
@@ -26,30 +26,30 @@ public class CabInvoiceGeneratorTest {
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare(distance, time, "NORMAL");
         Assertions.assertEquals(5, fare, 0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
         Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1)
+                new Ride(2.0, 5, "Normal"),
+                new Ride(0.1, 1, "Premium")
         };
         InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare(rides);
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 35.0);
         Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
     }
 
     @Test
     public void givenUserId_ShouldReturnInvoiceSummary() {
         Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1)
+                new Ride(2.0, 5, "Premium"),
+                new Ride(0.1, 1, "Premium")
         };
         cabInvoiceGenerator.userRideRepository.put("User001", Arrays.asList(rides));
         InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User001");
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 50.0);
         Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
     }
 }
