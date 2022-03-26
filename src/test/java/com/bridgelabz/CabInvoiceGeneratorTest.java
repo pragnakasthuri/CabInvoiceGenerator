7427068a590d3a70753b5a28e51964b327dc74bd
pragnakasthruri <kpragna25@gmail.com>
2022-03-26 07:14:16 +0530

    Adding test case to handle invalid user

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -42,14 +42,31 @@ public class CabInvoiceGeneratorTest {
     }
 
     @Test
-    public void givenUserId_ShouldReturnInvoiceSummary() {
+    public void givenUserId_whenValidUser_ShouldReturnInvoiceSummary() {
         Ride[] rides = {
                 new Ride(2.0, 5, "Premium"),
                 new Ride(0.1, 1, "Premium")
         };
         cabInvoiceGenerator.userRideRepository.put("User001", Arrays.asList(rides));
-        InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User001");
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 50.0);
-        Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
+        try {
+            InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User001");
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 50.0);
+            Assertions.assertEquals(invoiceSummary, expectedInvoiceSummary);
+        } catch (CabInvoiceCustomException e) {}
+    }
+
+    @Test
+    public void givenUserId_whenInValidUser_ShouldThrowException() {
+        Ride[] rides = {
+                new Ride(2.0, 5, "Premium"),
+                new Ride(0.1, 1, "Premium")
+        };
+        cabInvoiceGenerator.userRideRepository.put("User001", Arrays.asList(rides));
+        try {
+            InvoiceSummary invoiceSummary = cabInvoiceGenerator.calculateFare("User002");
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 50.0);
+        } catch (CabInvoiceCustomException e) {
+            Assertions.assertEquals(CabInvoiceCustomException.ExceptionType.INVALID_USER, e.type);
+        }
     }
 }
